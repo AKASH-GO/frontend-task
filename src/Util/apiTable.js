6ffe943062424d9116048c83d8fe7376262fc587
AKASH <88474098+AKASH-GO@users.noreply.github.com>
2023-12-22 11:16:34 +0530

    frontend

---
+++ src/Util/apiTable.js
@@ -0,0 +1,34 @@
+export const ApiTable = {
+    display: {
+        label: "data/"
+    },
+    sortasc: {
+        label: "data?sort=asc"
+    },
+    sortdesc: {
+        label: "data?sort=desc"
+    },
+    orgid: {
+        label: "&column_name=org_id"
+    },
+    orgname: {
+        label: "&column_name=org_name"
+    },
+    adslastsyncat: {
+        label: "&column_name=adslastsyncat"
+    },
+    inventorylastsyncat: {
+        label: "&column_name=inventorylastsyncat"
+    },
+    orderslastsyncat: {
+        label: "&column_name=orderslastsyncat"
+    },
+    paymentslastsyncat: {
+        label: "&column_name=paymentslastsyncat"
+    },
+    returnslastsyncat: {
+        label: "&column_name=returnslastsyncat"
+    },
+}
+export const METHOD = "get"
+export const BASEURL = "https://5729-2405-201-e015-b09c-fc83-94c9-905e-a111.ngrok-free.app/"
